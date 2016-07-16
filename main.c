d1fb385a84dc4c5da6c7df9b5e29d2ea4976aadf
Thomas <thomas.goeppel@gmail.com>
2016-07-16 22:12:02 +0200

    variable declarations in main.c updated

--- main.c
+++ main.c
@@ -7,15 +7,15 @@ static struct h {
   uint16_t sysDummy1;   //  2 (unknown)
   uint16_t sysDummy2;   //  4 (unknown)
   uint16_t sysBASE;     //  6 BASE (BASEE)
-  uint16_t SysTmp;      //  8 tmp 	(0)
-  uint16_t sysGtIN;     // 10 >IN	(0)
-  uint16_t sysHsTIB;    // 12 #TIB	(0)
-  uint16_t sysTIB;      // 14 TIB	(TIBB)
-  uint16_t sysEVAL;     // 16 'EVAL(INTER)
-  uint16_t sysHLD;      // 18 HLD	(0)
-  uint16_t sysCONTEXT;  // 20 CONTEXT pointer (LASTN)
-  uint16_t sysULAST;    // 22 ULAST (0)
-  uint16_t sysDummy24;  // 24 (unknown)
+  uint16_t sysEVAL;     //  8 'EVAL(INTER)
+  uint16_t sysCONTEXT;  // 10 CONTEXT pointer (LASTN)
+  uint16_t sysCP;       // 12 points to top of dictionary
+  uint16_t sysULAST;    // 14 ULAST (0)
+  uint16_t sysTIB;      // 16 TIB	(TIBB)
+  uint16_t sysNTIB;     // 18 #TIB	(0)
+  uint16_t sysGtIN;     // 20 >IN	(0)
+  uint16_t sysHLD;      // 22 HLD	(0)
+  uint16_t SysTmp;      // 24 tmp 	(0)
   uint16_t sysXTEMP;    // 26 also PROD1 ; ??? address called by CREATE
   uint16_t sysYTEMP;    // 28 also PROD2 ; ??? address called by CREATE
   uint16_t sysPROD3;    // 30 PROD1 .. PROD3 space for UM*
@@ -27,9 +27,9 @@ static struct h {
 
 uint8_t userram[90];  // $0-$7F User RAM memory, system variables
 uint8_t ctop[0x280];  // $80 Start of user defined words, linked to ROM dictionary
-uint8_t dstack[0x90]; // $380 Data stack, growing downward
-uint8_t tib;          // $390 Terminal input buffer TIB
-                        // $3FF Return stack, growing downward
+uint8_t dstack[0x82]; // $380 Data stack, growing downward
+uint8_t tib;          // $382 Terminal input buffer TIB
+                      // $3FF Return stack, growing downward
 
 void TIM4_IRQHandler() __interrupt (23);
 
