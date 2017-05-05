1d22293d000bd9d902885fe4d675e22cc29c61d2
Thomas <thomas.goeppel@gmail.com>
2017-05-05 20:29:32 +0200

    just some formatting

--- HC12/ezradio.fs
+++ HC12/ezradio.fs
@@ -1,30 +1,35 @@
 ( Work-in-progress library for the ezradio pro )
 FILE
+
 VARIABLE cmdbuf 128 ALLOT
-HEX
 : cs_delay ( -- )
-80 0 DO LOOP ;
-: cmdsend ( cmdlen -- ) 
-cs_delay
-SPIS
-cs_delay
-cmdbuf OVER 0
-DO
-  DUP C@ SPI 
-  OVER C!
-  1+
-LOOP
-DROP SPIE ; 
+  $80 0 DO LOOP ;
+
+: cmdsend ( cmdlen -- )
+  cs_delay  SPIS  cs_delay
+  cmdbuf OVER 0
+  DO
+    DUP C@ SPI
+    OVER C!
+    1+
+  LOOP
+  DROP SPIE ;
+
 : cmdprint ( cmdlen -- )
-cmdbuf OVER 0 DO
-  DUP C@ .
-  1+ 
-LOOP
-DROP ;
+  cmdbuf OVER 0 DO
+    DUP C@ .
+    1+
+  LOOP
+  DROP ;
+
 : cmdbufwipe ( cmdlen -- )
-cmdbuf OVER 0 DO
-  FF OVER C! 1+
-LOOP
-DROP ;
-: ezrinit ( -- ) EZD EZE SPIS ;
+  cmdbuf OVER 0 DO
+    $FF OVER C! 1+
+  LOOP
+  DROP ;
+
+: ezrinit ( -- )
+  EZD EZE SPIS ;
+
 HAND
+
