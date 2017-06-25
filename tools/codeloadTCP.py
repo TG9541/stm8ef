efa1fc2947a1411ced008b04591c04a10c84f3eb
Thomas <thomas.goeppel@gmail.com>
2017-06-25 11:26:07 +0200

    issue #32: use telnet library

--- tools/codeloadTCP.py
+++ tools/codeloadTCP.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python2
 
 import sys
-import time
-import socket
+import telnetlib
 
-so = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-so.connect(("localhost",10000))
-si = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-si.connect(("localhost",10001))
+HOST = "localhost"
+PORT = 10000
+tn = telnetlib.Telnet(HOST,PORT)
+tn.read_until("menu\r\n")
 
 if len(sys.argv) < 2:
     print('Usage %s <file1> ... [fileN]' % (sys.argv[0]))
@@ -16,21 +15,15 @@ if len(sys.argv) < 2:
 def upload(path): 
     with open(path) as source:
         for line in source.readlines():
-            time.sleep(0.2)        
             line = line.strip()
             if not line: continue
             if len(line) > 64:
                 raise 'Line is too long: %s' % (line)
             print('sending: ' + line)
-            so.sendall(line)
-            so.sendall('\n')
-            chin = ''
-            response_buffer = []
-            while chin <> '\v':
-	      response_buffer.append(chin)
-	      chin = si.recv(1)
-            response = ''.join(response_buffer)
-            sys.stdout.write(response)
+            line = line.strip()
+            if not line: continue
+            tn.write(line + '\r')
+            print tn.expect(['(\v|\n)'])[2]
 
 for path in sys.argv[1:]:
     print('Uploading %s' % path)
