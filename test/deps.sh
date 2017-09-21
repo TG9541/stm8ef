20c2c4b937d2b3c6a5d222162b7d69829487e0a8
Lars Brinkhoff <lars@nocrew.org>
2017-09-21 11:55:45 +0200

    Make Travis cache the SDCC binaries between builds.

--- test/deps.sh
+++ test/deps.sh
@@ -1,6 +1,8 @@
+test -f $HOME/sdcc/bin/sdasstm8 && exit 0
+
 sudo apt-get install subversion libboost-dev texinfo
 svn checkout svn://svn.code.sf.net/p/sdcc/code/trunk/sdcc sdcc
 cd sdcc
-./configure --disable-pic14-port --disable-pic16-port
+./configure --disable-pic14-port --disable-pic16-port --prefix=$HOME/sdcc
 make
 sudo make install
