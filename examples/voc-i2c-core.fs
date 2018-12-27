ae2b5375884fb7b55d88f5dcb532d368d55193f5
Thomas <thomas.goeppel@gmail.com>
2018-12-27 15:25:35 +0100

    2018 version of VOC I2C example

--- examples/voc-i2c-core.fs
+++ examples/voc-i2c-core.fs
@@ -1,85 +1,90 @@
 \ stm8ef : voc-i2c-core.fs                                             MM-170928
 \ ------------------------------------------------------------------------------
-\             I2C Library, Core Word Set, Bit-Bang Implementation
+\             I2C Bus Master, Core Word Set, Bit-Bang Implementation
 \
-\               Copyright (C) 2017 manfred.mahlow@forth-ev.de
+\               Copyright (C) 2017,2018 manfred.mahlow@forth-ev.de
 \
-\ Requires: * STM8S eForth on STM8S103xy
-\           * e4thcom -t stm8ef
+\           License see github.com/TG9541/stm8ef/blob/master/LICENSE.md
+\
+\ Requires: * STM8S eForth on an STM8S MCU (tested with STM8S103xy  STM8S105xy)
+\           * e4thcom Terminal (e4thcom-x.y.z -t stm8ef) or codeload.py
 \
 \ Uses: PB5 as serial data pin (SDA)
 \       PB4 as serial clock pin (SCL)
 
-\ I2C clock frequency : ?
+\ I2C clock frequency : ~ 50 kHz with 8 MHz MCU clock
 \
-\ External pull-up resistors of 10 kOhm are required on the SDA and SCL pins.
+\ External pull-up resistors of ~ 10 kOhm are required on the SDA and SCL pins.
 \
 
-#require VOC \ requires the persistent patch for context switching, see CURRENT
-
-#require CONSTANT
+#require VOC
 #require :NVM
 #require ALIAS
 #require ]B!
+#require ]B?
+#require WIPE
 
 \res MCU: STM8S103
 
 RAM
 
-\res export PB_IDR  PB_ODR  PB_DDR
-\res export BIT5
+\res export PB_IDR  PB_ODR  PB_DDR   PB_CR1  PB_CR2
 
-:NVM ( -- ) [ 0 PB_DDR 5 ]B! ;RAM      ALIAS SDA1
-:NVM ( -- ) [ 1 PB_DDR 5 ]B! ;RAM      ALIAS SDA0
-:NVM ( -- f ) PB_IDR C@ BIT5 AND ;RAM  ALIAS SDA?
-:NVM ( -- ) [ 0 PB_DDR 4 ]B! ;RAM      ALIAS SCL1
-:NVM ( -- ) [ 1 PB_DDR 4 ]B! ;RAM      ALIAS SCL0
+:NVM ( -- ) dup drop ;RAM ALIAS WAIT
+
+:NVM ( -- ) [ 0 PB_DDR 4 ]B! WAIT ;RAM  ALIAS SCL1
+:NVM ( -- ) WAIT [ 1 PB_DDR 4 ]B! ;RAM  ALIAS SCL0
+
+:NVM ( -- ) [ 0 PB_DDR 5 ]B! WAIT ;RAM  ALIAS SDA1
+:NVM ( -- ) WAIT [ 1 PB_DDR 5 ]B! ;RAM  ALIAS SDA0
+
+:NVM ( -- f ) WAIT [ PB_IDR 5 ]B? ;RAM  ALIAS SDA?
 
 NVM
 
 VOC i2c  i2c DEFINITIONS
 
-: init ( -- )
-\ Initialize the I2C Bus interface
-  SDA1 SCL1                          \ enable I2C pins as input
-  [ 0 PB_ODR 5 ]B! [ 0 PB_ODR 4 ]B!  \ set output registers low
-;
-
-\ : wait ( -- ) ;
-
 : start ( -- ) \ in: SDA=? SCL=? out: SDA=SCL=0
 \ Start an I2C Bus transmission.
-  SDA1 SCL1 ( wait ) SDA0 ( wait ) SCL0 ;
+  SDA1 SCL1 SDA0 SCL0 ;
 
-: stop ( -- )
+: stop ( -- ) \ in: SDA=1 SCL=0  out: SDA=SCL=1
 \ Stop an I2C Bus transmission.
-  SDA0 ( wait ) SCL1 ( wait ) SDA1 ;
+  SDA0 SCL1 SDA1 ;
 
 : tx ( byte -- )
 \ Send a byte to the I2C Bus.
-  7 FOR DUP $80 AND IF SDA1 ELSE SDA0 THEN ( wait ) SCL1 ( wait ) SCL0 2* NEXT
-  DROP ;
+  DUP 7 FOR $80 AND IF SDA1 ELSE SDA0 THEN SCL1 2* DUP SCL0 NEXT 2DROP ;
 
 : nak? ( -- f ) \ in: SDA=? SCL=0  out: SDA=1 SCL=0
 \ Return a true/false flag if an NAK/ACK was received from the I2C Bus.
-  SDA1 ( wait ) SCL1 ( wait ) SDA? SCL0 ;
+  SDA1 SCL1 SDA? SCL0 ;
 
 : rx ( -- byte ) \ in: SDA=1 SCL=0   out: SDA=?  SCL=0
 \ Receive a byte from the I2C Bus.
-  0 7 FOR ( wait ) SCL1 ( wait ) SDA? SCL0 IF 1 ELSE 0 THEN SWAP 2* OR NEXT ;
+  0 7 FOR 2* SCL1 SDA? SCL0 ABS OR NEXT ;
 
 : nak ( -- ) \ in: SDA=? SCL=0  out: SDA=1 SCL=0
 \ Send an NAK to the I2C Bus.
- SDA1 ( wait ) SCL1 ( wait ) SCL0 ;
+  SDA1 SCL1 SCL0 ;
 
 : ack ( -- ) \ in: SDA=? SCL=0  out: SDA=1 SCL=0
 \ Send an ACK to the I2C Bus.
-  SDA0 ( wait ) SCL1 ( wait ) SCL0 SDA1 ;
+  SDA0 SCL1 SCL0 SDA1 ;
 
 : rdy ( sid -- f )
 \ Send a start/stop sequence with the slave address sid and return true if an
 \ ACK is returned. Otherwise return a false flag.
   2*  i2c start  i2c tx  i2c nak? 0=  i2c stop ;
+
+: init ( -- )
+\ Initialize the I2C Bus interface
+  SDA1 SCL1                        \ enable I2C pins as input
+  [ 0 PB_ODR 5 ]B! [ 0 PB_ODR 4 ]B!  \ set output registers low
+  \ Send a start/stop sequence. Otherwise the first bus access fails with an
+  \ ACK error
+  i2c start i2c stop
+;
 
 FORTH DEFINITIONS
 
@@ -87,11 +92,9 @@ RAM WIPE
 
 i2c init
 
-\index  i2c rdy .  i2c init  i2c WORDS
-
 \ ------------------------------------------------------------------------------
-\ Last Revision: MM-171121  B! --> ]B!
-
-
-
-
+\ Last Revision: TG951 ]B? parameter order changed
+\                MM-181221 V1.1 , ]B? and WAIT added
+\                MM-171206 start/stop sequence added to i2c init
+\                MM-171121 B! --> ]B!
+\                MM-170928 ported from MSP430 Version
--- examples/voc-i2c-eeprom.fs
+++ examples/voc-i2c-eeprom.fs
@@ -14,13 +14,8 @@ VOC eeprom  i2c eeprom DEFINITIONS
 
   $50 CONSTANT sid
 
-  : C@ ( a -- c )  1 SWAP i2c eeprom sid i2c read ;
+  : C@ ( a -- c )  1 i2c eeprom sid i2c read ;
 
   : C! ( c a -- )  2 i2c eeprom sid i2c write ;
 
 FORTH DEFINITIONS
-
-RAM WIPE
-
-\index  i2c init  i2c eeprom WORDS  i2c WORDS
-
